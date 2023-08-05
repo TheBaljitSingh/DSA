c1bccbea2e0bd1bc385e2f3143f15fc19c6fc872
Baljit Singh <baljitsingh692002@gmail.com>
2023-08-05 23:34:07 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,11 +1,9 @@
  void bubbleSort(int arr[], int n)
     {
-       //code here
        for(int i=0; i<n; i++){
            for(int j=i+1; j<n; j++){
                if(arr[i]>=arr[j]){
                    swap(arr[i],arr[j]);
-
                }
            }
            
